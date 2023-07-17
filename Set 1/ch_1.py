e9ca2f9238da8dffe106ae27f7e54a70d5e321bf
Docik99 <docikpocik@gmail.com>
2023-07-17 13:39:04 +0300

    add assert

--- Set 1/ch_1.py
+++ Set 1/ch_1.py
@@ -1,6 +1,12 @@
 import base64
 
-if __name__ == '__main__':
-    secret = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+def bs(secret):
     secret = bytes.fromhex(secret)
-    print(base64.b64encode(secret))
+    return base64.b64encode(secret).decode('utf-8')
+
+
+if __name__ == '__main__':
+    assert bs("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") == \
+           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    print("Passed")
