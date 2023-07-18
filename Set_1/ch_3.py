686948ac273e1dfaecd55136a32d5b3e5a1792fa
Docik99 <docikpocik@gmail.com>
2023-07-18 16:49:49 +0300

    pep8

--- Set_1/ch_3.py
+++ Set_1/ch_3.py
@@ -1,7 +1,16 @@
 CHARACTER_FREQ = {
-    'e': 12.6, 't': 9.37, 'a': 8.34, 'o': 7.7, 'n': 6.8, 'i': 6.71,
-    'h': 6.11, 's': 6.11, 'r': 5.68, 'l': 4.24, 'u': 2.85, ' ': 15
-}
+    'e': 12.6,
+    't': 9.37,
+    'a': 8.34,
+    'o': 7.7,
+    'n': 6.8,
+    'i': 6.71,
+    'h': 6.11,
+    's': 6.11,
+    'r': 5.68,
+    'l': 4.24,
+    'u': 2.85,
+    ' ': 15}
 
 
 def get_score(input_bytes):
@@ -43,13 +52,15 @@ def brute_force(secret):
 def print_result(results):
     for result in results:
         try:
-            print(f"{result['text'].decode()}   Score: {int(result['score'])}    Key: {chr(result['key'])}")
-        except:
+            print(
+                f"{result['text'].decode()}   Score: {int(result['score'])}    Key: {chr(result['key'])}")
+        except BaseException:
             print("'utf-8' codec can't decode byte")
 
 
 def main():
-    secret = bytes.fromhex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+    secret = bytes.fromhex(
+        "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
     results = brute_force(secret)
     print_result(results)
 
