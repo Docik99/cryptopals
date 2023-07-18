238458f0ed68d6c499f43e20fc153b74d4b8aed3
Docik99 <docikpocik@gmail.com>
2023-07-18 16:50:15 +0300

    pep8

--- Set_1/ch_1.py
+++ Set_1/ch_1.py
@@ -8,5 +8,5 @@ def bs(secret):
 
 if __name__ == '__main__':
     assert bs("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") == \
-           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
     print("Passed")
