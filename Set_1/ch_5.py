7052ccb6aea8275f1ae0c11f1b6f9d3355541336
Docik99 <docikpocik@gmail.com>
2023-07-21 10:21:59 +0300

    done ch_5

--- Set_1/ch_5.py
+++ Set_1/ch_5.py
@@ -1,14 +1,21 @@
-def hex_xor(hexdata1, hexdata2):
-    dec1 = int(hexdata1, 16)
-    dec2 = int(hexdata2, 16)
-    xor = dec1 ^ dec2
-    return hex(xor)[2:]
+def xor(text, key):
+    newtext = b''
+    i = 0
+
+    for byte in text:
+        newtext += bytes([byte ^ key[i]])
+
+        i = i + 1 if (i < len(key) - 1) else 0
+
+    return newtext.hex()
 
 
 def main():
-    data1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-    key = "ICE"
-    hex_xor(data1, key)
+    assert xor(
+        b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+        b"ICE") == "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a262263242727652" \
+                   "72a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    print("Passed")
 
 
 if __name__ == "__main__":
